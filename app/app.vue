3b11214cbb03ece91d433d1ec7cdfabaf21f0711
Muneeb <freelancermuneeb@gmail.com>
2026-05-30 16:04:57 +0500

    feat: app layout with dynamic head and configure MCP servers for Nuxt and better-auth

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,13 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk
+      ? `${titleChunk} | Better Nuxt`
+      : 'Better Nuxt'
+  },
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
